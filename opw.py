57d8cc1f2f50c44461e2c20d664a075463825f54
Akshay Singh Chauhan <142593499+akshaytheGodxo@users.noreply.github.com>
2023-09-20 17:19:02 +0530

    Update Gully Boy program.py

--- opw.py
+++ opw.py
@@ -1,3 +1,20 @@
+'''
+In this Programme , We have to take 3 things as input from the User : 
+1. The main String value
+2. The place holder Value
+3. Value to repeat the new reversed integer 
+
+We will use the place holder value to slice our string in two part and then we will swap the place values of both substrings 
+
+Then we will repeatidly print the reversed substring value 
+
+We can also use array / list method to reverse it as it becomes easier
+
+Note. 
+1. Make sure there is space between all of the letters in the final String 
+2. Also if there is a space in the inital string , you have to ignore that :D
+Enjoy the Code :D
+'''
 c = input()
 m = int(input())
 n = int(input())
